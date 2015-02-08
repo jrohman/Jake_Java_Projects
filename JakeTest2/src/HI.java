2990d747ecfbe0b367ebcc342eab80792ce64961
jrohman <jwrohman@sbcglobal.net>
2015-02-08 12:56:48 -0600

    I am learning stuff

---
+++ JakeTest2/src/HI.java
@@ -0,0 +1,17 @@
+
+public class HI {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+		for (int i = 0; i < 10; i++) {
+
+			for (int j = 0; j < 10; j++) {
+				if (true) {
+					// Do nothing
+				}
+			}
+		}
+	}
+
+}
